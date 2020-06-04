c252bdaacd9b9b2b12deb7fac36245df2abd1d43
소원 박 <s2018s29@e-mirim.hs.kr>
2020-06-04 14:49:34 +0900

    오버로딩

--- overridemethos/Program.cs
+++ overridemethos/Program.cs
@@ -8,6 +8,37 @@ namespace overridemethos
 {
     class Program
     {
+        int Power(int input)
+        {
+            return input * input;
+        }
+
+        int Power(int input, int count)
+        {
+            for (int i = 0; i < count; i++) input *= input;
+            return input;
+        }
+
+        int SumAll(int end)
+        {
+            int res = 0;
+            for(int i = 0; i < end; i++)
+            {
+                res += i;
+            }
+            return res;
+        }
+
+        int SumAll(int start, int end)
+        {
+            int res = 0;
+            for (; start < end; start++)
+            {
+                res += start;
+            }
+            return res;
+        }
+
         static void Main(string[] args)
         {
         }
